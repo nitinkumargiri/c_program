dc786dbcab0bbab906b3f50f8c3755479063b3f4
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-10-07 21:53:32 +0530

    calculator in c

---
+++ first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+   // int a = 10;
+   // int b =20;
+    printf ("hello world");
+    printf("\nhi how are you");
+    printf("\nmy name is abhi");
+    return 0;
+}
---
+++ second.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int a = 10;
+    int b = 40;
+    printf("%d",a);
+    printf("%d",b);
+    return 0;
+}
