7d347f71bd1aada563d4ff0200cd332f494ec19d
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-10-09 15:53:49 +0530

    rock_paper_scissor_game

---
+++ rock_paper_scissor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(){
+    int userchoice , computerchoice;
+    printf("welcome to rock, paper, scissor game\n");
+    printf("chose1. rock\nchose2. paper\nchose3.scissor\n");
+    printf("enter your choice (1-3): ");
+    scanf("%d",&userchoice);
+
+    srand(time(0));
+    computerchoice = (rand() % 3) +1;
+
+    printf("you chose: %d\n", userchoice);
+    printf("computer choice: %d\n",computerchoice);
+    
+    if(userchoice == computerchoice){
+        printf("it's a draw!\n");
+    }else if((userchoice == 1 && computerchoice == 3)||
+(userchoice == 2 && computerchoice == 1)||
+(userchoice == 3 && computerchoice == 2)){
+    printf("you winner.......");
+}else{
+    printf("you lose! computer win......");
+}
+return 0;
+}
