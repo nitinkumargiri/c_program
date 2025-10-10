bef8289fa52ba7673550ed16fe99f06772c6d62e
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-10-10 21:24:07 +0530

    temperature_mesurement

--- temperature_mesurment.c
+++ temperature_mesurment.c
@@ -3,11 +3,18 @@ int main(){
     
     // Temperature mesure in celcius to fehrenheit.
 
-    int temperature;
-    printf("Enter to temperature to celcius: ");
-    scanf("%d",temperature);
-    float find = (temperature * 9/5) + 32;
-    printf("the temperature in fahrenheit is: %f",find);
+    int celcius , fehrenheit;
+    printf("Enter the temperature to celcius: ");
+    scanf("%d",&celcius);
+    float find = (celcius * 9/5) + 32;
+    printf("the temperature in fahrenheit is: %f\n\n",find);
+
+    // temprature in fehrenheit to celcius.
+
+    printf("Enter the temperature in fehrenheit: ");
+    scanf("%d",&fehrenheit);
+    float temp = 5/9 * (fehrenheit - 32);
+    printf("The temperature in celcius is : %f", temp);
 
     return 0;
 }
