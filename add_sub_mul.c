c586cc31da545ee1c4c29089361b86bffbc7dd04
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-10-10 21:45:11 +0530

    find_number

--- add_sub_mul.c
+++ add_sub_mul.c
@@ -1,55 +1,55 @@
-#include <stdio.h>
-int main (){
-    //take  two input from user after we will substract
-    printf("FOR SUBSTRACT VALUE..\n\n");
-    int num1 ,num2 ,sub;
-    printf ("enter your first number: ");
-    scanf("%d",&num1);
-    printf("enter your second number: ");
-    scanf("%d", &num2);
-    sub = num1 - num2;
-    printf("YOUR RESULT : %d\n\n",sub);
+// #include <stdio.h>
+// int main (){
+//     //take  two input from user after we will substract
+//     printf("FOR SUBSTRACT VALUE..\n\n");
+//     int num1 ,num2 ,sub;
+//     printf ("enter your first number: ");
+//     scanf("%d",&num1);
+//     printf("enter your second number: ");
+//     scanf("%d", &num2);
+//     sub = num1 - num2;
+//     printf("YOUR RESULT : %d\n\n",sub);
 
-    //take two input  from user after we will add. 
-    printf("FOR ADD VALUE..\n\n");
-    int num0, num9, sum;
-    printf("enter your first number: ");
-    scanf("%d",&num0);
-    printf("enter your second number: ");
-    scanf("%d",&num9);
+//     //take two input  from user after we will add. 
+//     printf("FOR ADD VALUE..\n\n");
+//     int num0, num9, sum;
+//     printf("enter your first number: ");
+//     scanf("%d",&num0);
+//     printf("enter your second number: ");
+//     scanf("%d",&num9);
 
-    //now we will sum num1 + num2
-    sum = num0 + num9;
-    printf("the sum of  your number is %d\n\n",sum);
+//     //now we will sum num1 + num2
+//     sum = num0 + num9;
+//     printf("the sum of  your number is %d\n\n",sum);
     
-    //mulltiply the two value together
-    printf("FOR MULTIPLY VALUE...\n\n");
-    int a ,b ,c;
-    printf("enter first number: ");
-    scanf("%d",&a);
-    printf("enter your second no: ");
-    scanf("%d", &b);
+//     //mulltiply the two value together
+//     printf("FOR MULTIPLY VALUE...\n\n");
+//     int a ,b ,c;
+//     printf("enter first number: ");
+//     scanf("%d",&a);
+//     printf("enter your second no: ");
+//     scanf("%d", &b);
 
-    c = a * b;
-    printf("the result is : %d\n\n", c);
+//     c = a * b;
+//     printf("the result is : %d\n\n", c);
 
-    //Devide the two value together
-    printf("FOR division VALUE...\n\n");
-    int x ,y ,div;
-    printf("enter first number: ");
-    scanf("%d",&x);
-    printf("enter your second no: ");
-    scanf("%d", &y);
+//     //Devide the two value together
+//     printf("FOR division VALUE...\n\n");
+//     int x ,y ,div;
+//     printf("enter first number: ");
+//     scanf("%d",&x);
+//     printf("enter your second no: ");
+//     scanf("%d", &y);
 
-    if (y != 0)
-    {
-        div = x /y;
-        printf("the result is : %d\n",div);
+//     if (y != 0)
+//     {
+//         div = x /y;
+//         printf("the result is : %d\n",div);
             
-    }else{
-        printf("ERROR: devided by zero");
-    }
+//     }else{
+//         printf("ERROR: devided by zero");
+//     }
     
     
-    return 0;
-}
+//     return 0;
+// }
---
+++ find_odd_even.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+
+    // Finding a odd even number in this program.
+
+    int number;
+    printf("Enter your number : ");
+    scanf("%d", &number);
+    if (number %2 == 0){
+        
+        printf("Your number is even...");
+
+    }else{
+        printf("Yoyr number is odd ....\n\n");
+    }
+
+    return  0;
+
+    
+}
