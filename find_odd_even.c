43c4c90aac7eef8940178a97de6caee777a13aad
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-10-10 21:47:23 +0530

    find number

--- find_odd_even.c
+++ find_odd_even.c
@@ -8,11 +8,24 @@ int main(){
     scanf("%d", &number);
     if (number %2 == 0){
         
-        printf("Your number is even...");
+        printf("Your number is even...\n\n");
 
     }else{
         printf("Yoyr number is odd ....\n\n");
     }
+
+
+    printf("Enter your number : ");
+    scanf("%d", &number);
+
+    if (number %2 == 0){
+
+        printf("Your number is even...");
+
+    }else{
+        printf("Yoyr number is odd ....");
+    }
+
 
     return  0;
 
