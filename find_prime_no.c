71fb846760a3e18eb3acd51e48f56ef21b6feb4b
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-11-16 21:31:27 +0530

    prime_no

--- find_prime_no.c
+++ find_prime_no.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 int main(){
-    int num, i;
+    int num, i, flag;
     printf("enter your number ");
     scanf("%d",&num);
+
+    if (num <= 1)
+    {
+        printf("NOt a  prime number");
+    }
+
+    for (int i = 2; i < num; i++)
+    {
+        if (num %i == 0)
+        {
+            flag = 1;
+            break;
+        }
+        
+    }if (flag == 0)
+    {
+        printf("prime number");
+
+    }else{
+        printf("not a prime number");
+    }
+
+    return 0;
+    
+    
+    
 
 }
