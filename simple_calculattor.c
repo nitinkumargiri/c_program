f515d3583589335342a0d00b220e0a4a66b188d2
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-11-17 21:05:41 +0530

    cal

---
+++ simple_calculattor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+    int a , b;
+    char operator;
+    printf("enter your number : ");
+    scanf("%d",&a);
+    printf("enter your second no : ");
+    scanf("%d", &b);
+    printf("enter your choice (+,-,*,/) : ");
+    scanf("%d",operator);
+
+    if (operator = '+')
+    {
+        printf("%d",a + b);
+    }
+    else if (operator = '-')
+    {
+        printf("%d",a - b);
+        
+    }
+    
+}
