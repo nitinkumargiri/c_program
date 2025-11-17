59f8397d152726cbea6df715dfd9700fac8ec843
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-11-17 09:44:37 +0530

    factorial

--- find_factorial.c
+++ find_factorial.c
@@ -8,5 +8,21 @@ int main (){
      printf("enter your number : ");
      scanf("%d", &n);
 
+     if (n < 0)
+     {
+        printf("factorianl are does ni exist negative no\n");
+    
+     }
+     for (int i = 1; i <= n; i++)
+     {
+        fact = fact * i;
+        
+     }
+     printf("the factorial of %d is %d", n,fact);
+
+     return 0;
      
+     
+
+
 }
