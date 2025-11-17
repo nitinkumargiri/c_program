439eb3526bf99b999df53d8850403a87ff8e579b
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-11-17 09:30:26 +0530

    factor

---
+++ find_factorial.c
@@ -0,0 +1,12 @@
+// write a program to find  factorial of a given no.
+
+#include <stdio.h>
+int main (){
+    int n, i;
+     long long fact = 1;
+
+     printf("enter your number : ");
+     scanf("%d", &n);
+
+     
+}
