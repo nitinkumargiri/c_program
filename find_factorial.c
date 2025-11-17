f4710349017814b6a962ec89dba6db23699659f7
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-11-17 20:20:53 +0530

    factorial

--- find_factorial.c
+++ find_factorial.c
@@ -1,6 +1,6 @@
 // write a program to find  factorial of a given no.
 
-#include <stdio.h>
+/*#include <stdio.h>
 int main (){
     int n, i;
      long long fact = 1;
@@ -25,4 +25,25 @@ int main (){
      
 
 
+}*/
+#include <stdio.h>
+int main(){
+   int n,i,fact = 1;
+   printf("enter your no: ");
+   scanf("%d",&n);
+
+   if (n <= 0)
+   {
+      printf("not factorial");
+      
+   }
+   for (int i = 1; i <= n; i++)
+   {
+      fact = fact*i;
+   }
+   printf("the factorial of %d is %d",n,fact);
+   
+   return 0;
 }
+
+   
