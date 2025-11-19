81cb6b92941587c3cf07dc0e7ce0c9d49b3a6a3b
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-11-19 10:44:53 +0530

    oddeven

---
+++ program_tryal.c
@@ -0,0 +1,19 @@
+//odd even
+#include <stdio.h>
+int main(){
+    int a;
+    printf ("enter your no: ");
+    scanf("%d",a);
+
+    if (a %2 == 0)
+    {
+        printf("your no is even");
+        
+    }else{
+        printf("your no is odd");
+    }
+    
+
+
+    return 0;
+}
