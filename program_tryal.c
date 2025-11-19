88d5a5455d1ab71af6d3e0360e2269f0ff6ebe2c
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-11-19 20:05:56 +0530

    try

--- program_tryal.c
+++ program_tryal.c
@@ -1,19 +1,8 @@
-//odd even
+
 #include <stdio.h>
 int main(){
-    int a;
-    printf ("enter your no: ");
-    scanf("%d",&a);
+    float celsius,fahrenheit;
+    printf("enter your number : ");
 
-    if (a %2 == 0)
-    {
-        printf("your no is even");
-        
-    }else{
-        printf("your no is odd");
-    }
-    
-
-
-    return 0;
 }
+    
