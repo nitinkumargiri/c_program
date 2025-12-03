8eeda3851f683512cd2be06d3de6b150931c63e7
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-12-03 21:18:45 +0530

    chatboot

--- chatbot.c
+++ chatbot.c
@@ -86,7 +86,7 @@ int main (){
         printf("the prime minister of india is __narendra modi\n");
     }if (strstr(input,"who is the first prime minister of india") != NULL || strstr(input,"first prime ministr of india") != NULL){
         printf("first prime minister of india is __Dr. jawaharlal nehru\n");
-    }else if (strstr(input,"prime minister of usa") != NULL){
+    }else if (strstr(input,"prime minister of usa") != NULL || strstr(input,"who is the prime minister of america") != NULL){
         printf("the prime minister of USA / America is __Donald trump\n");
     }
     
