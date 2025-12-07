8664f3558828f32b087325c358bb0a48cf56e720
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-12-07 19:05:20 +0530

    result_grade_desider

--- get_marks_grade.c
+++ get_marks_grade.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 int main (){
     int mark;
+    printf("=== WELCOME ===\n");
     printf("enter your number : ");
     scanf("%d",&mark);
 
     if (mark <= 100 && mark >= 80)
     {
         printf("first ");
-        /* code */
+        
     }else if (mark < 80 && mark >= 60)
     {
         printf("second ");
-        /* code */
+        
     }else if (mark < 60 && mark >=30)
     {
         printf("third");
-        /* code */
+        
     }else{
         printf("Invailed input...");
     }
