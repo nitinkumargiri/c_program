aa249a5428812c18f4e893cedd4c1061f9e2b61c
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-12-08 21:22:40 +0530

    drow

--- trial.c
+++ trial.c
@@ -4,5 +4,25 @@ int main (){
     printf("enter your value : ");
     scanf("%d",&n);
 
+    for (int i = 1; i < n; i++)
+    {
+        for (int  j = 1; j<n; j++)
+        {
+            if (i==1||i==n||j==1||j==n||i==j||j+i==n+1)
+            {
+                printf("* ");
+                /* code */
+            }else{
+                printf("  ");
+            }
+            
+            /* code */
+        }
+        printf("\n");
+        
+        /* code */
+    }
+    return 0;
     
+
 }
