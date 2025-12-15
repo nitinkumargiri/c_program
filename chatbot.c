96cb7921fe3f633ee54ad82fbdb1a5b65999de93
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-12-15 16:24:40 +0530

    hi

--- chatbot.c
+++ chatbot.c
@@ -106,6 +106,8 @@ int main (){
     }else if (strstr(input,"national anthem of india is")!= NULL||strstr(input,"national anthem of india")!= NULL
     ||strstr(input,"national song of india")!=NULL){
         printf("jan gan man__ is the national anthem of india\n");
+    }if (strstr(input,"top exam in india is")!= NULL){
+        printf("IIT\n");
     }
     
     
