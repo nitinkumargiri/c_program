5500792f3fda28ad774ccf9c012a4a319646dff2
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-12-23 19:39:56 +0530

    counting_print

--- do_while.c
+++ do_while.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 int main(){
-    int x = 20;
+    int num, index = 0;
     printf ("enter your number : ");
+    scanf("%d",&num);
+    do
+    {
+        printf("%d\n",index);
+        index =index + 1;
+        /* code */
+    } while (index < num);
+    
 }
